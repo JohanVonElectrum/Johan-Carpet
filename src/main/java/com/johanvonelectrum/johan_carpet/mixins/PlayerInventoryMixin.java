b53194c8b36a848f606aaa0cf2e0fc657cef5d3d
JohanVonElectrum <joanestradium@gmail.com>
2022-01-02 21:40:42 +0100

    StackableShulkerInsert dupe bug fix
    Fixes #36

--- src/main/java/com/johanvonelectrum/johan_carpet/mixins/PlayerInventoryMixin.java
+++ src/main/java/com/johanvonelectrum/johan_carpet/mixins/PlayerInventoryMixin.java
@@ -3,11 +3,8 @@ package com.johanvonelectrum.johan_carpet.mixins;
 import com.johanvonelectrum.johan_carpet.JohanSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.EnderChestInventory;
-import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
@@ -18,32 +15,74 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
 
     PlayerInventory inventory = (PlayerInventory) (Object) this;
 
     @Shadow protected abstract int addStack(int slot, ItemStack stack);
-    @Shadow protected abstract boolean canStackAddMore(ItemStack existingStack, ItemStack stack);
 
     @Inject(method = "addStack(Lnet/minecraft/item/ItemStack;)I", at = @At("HEAD"), cancellable = true)
     private void addStack(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
         int first = inventory.getOccupiedSlotWithRoomForStack(stack);
+        if (first == -1) {
+            List<Byte>[] map = scanInventory();
+            if (map[1].isEmpty())
+                first = map[0].isEmpty() ? -1 : map[0].get(0);
+            else if (JohanSettings.shulkerInsert && (JohanSettings.shulkerInception || !(Block.getBlockFromItem(stack.getItem()) instanceof ShulkerBoxBlock))) {
+                for (byte i: map[1]) {
+                    ItemStack itemStack = inventory.main.get(i);
+                    if (stack.isEmpty() || addStackToShulker(itemStack, stack, map[0].isEmpty() ? -1 : map[0].get(0)))
+                        break;
+                }
+            } else
+                first = map[0].isEmpty() ? -1 : map[0].get(0);
+        }
         if (first != -1)
             stack.setCount(this.addStack(first, stack));
-        for(int i = 0; i < inventory.main.size() && !stack.isEmpty(); i++) {
-            ItemStack itemStack = inventory.main.get(i);
-            if (itemStack.isEmpty() || this.canStackAddMore(itemStack, stack))
-                stack.setCount(this.addStack(i, stack));
-            else if (!(Block.getBlockFromItem(stack.getItem()) instanceof ShulkerBoxBlock) && JohanSettings.shulkerInsert && Block.getBlockFromItem(itemStack.getItem()) instanceof ShulkerBoxBlock)
-                stack = addStackToShulker(itemStack, stack);
-        }
+
         cir.setReturnValue(stack.getCount());
     }
 
-    private ItemStack addStackToShulker(ItemStack shulker, ItemStack stack) {
+    private List<Byte>[] scanInventory() {
+        List<Byte>[] map = new ArrayList[] {new ArrayList<>(), new ArrayList<>(), new ArrayList<>()};
+        for (byte i = 0, j; i < inventory.main.size(); i++) {
+            ItemStack itemStack = inventory.main.get(i);
+            if (itemStack.isEmpty())
+                j = 0;
+            else if (Block.getBlockFromItem(itemStack.getItem()) instanceof ShulkerBoxBlock)
+                j = (byte) (itemStack.getCount() == 1 ? 1 : 2);
+            else
+                continue;
+            map[j].add(i);
+        }
+        map[1].addAll(map[2]);
+        return new List[] { map[0], map[1] };
+    }
+
+    private boolean addStackToShulker(ItemStack shulker, ItemStack stack, int emptySlot) {
+        ItemStack target = shulker;
+        System.out.println(target.getCount() + " : " + emptySlot);
+
+        if (shulker.getCount() > 1) {
+            if (emptySlot == -1)
+                return true;
+            else {
+                shulker.decrement(1);
+                ItemStack newShulker = shulker.getItem().getDefaultStack();
+                newShulker.setCount(1);
+                addStack(emptySlot, newShulker);
+                target = inventory.main.get(emptySlot);
+            }
+        }
+
+        System.out.println(target.getCount() + " : " + emptySlot);
+
         EnderChestInventory shulkerInventory = new EnderChestInventory();
-        NbtCompound shulkerTag = shulker.getNbt();
+        NbtCompound shulkerTag = target.getNbt();
         if (shulkerTag != null && shulkerTag.contains("BlockEntityTag", 10)) {
             NbtCompound blockEntityTag = shulkerTag.getCompound("BlockEntityTag");
             if (blockEntityTag.contains("Items", 9)) {
@@ -64,8 +103,8 @@ public abstract class PlayerInventoryMixin {
         }
         ItemStack remaining = shulkerInventory.addStack(stack);
         ((NbtCompound)shulkerTag.get("BlockEntityTag")).put("Items", shulkerInventory.toNbtList());
-        shulker.setNbt(shulkerTag);
-        return remaining;
+        target.setNbt(shulkerTag);
+        return remaining.isEmpty();
     }
 
 }
